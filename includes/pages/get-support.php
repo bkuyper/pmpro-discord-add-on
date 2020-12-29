b95b9f4bb8e691133b775340a3834d2348ce0194
aashishets <aashish.meena.ets@gmail.com>
2020-12-29 16:12:47 +0000

    chnage support page content and integrated with previous functionality on skype button add link to open skype chat

--- includes/pages/get-support.php
+++ includes/pages/get-support.php
@@ -1,76 +1,75 @@
-<div class="ets-details"> 
-	<div class="ets-com-logo">
-		<div class="ets-co-logo" > 
-			<img src= <?php echo ETS_PMPRO_DISCORD_URL."assets/images/user-original.png;"?> > 
-		</div>
-	</div>
-	<div class="ets-detail-dec"> 
-		<h2><?php echo __( "ExpressTech Software Solutions Pvt. Ltd.","ets_pmpro_discord" ); ?></h2>
-		<a href="https://www.expresstechsoftwares.com/">
-		<?php echo __( "ExpressTech Software Solutions Pvt. Ltd.", "ets_pmpro_discord" ); ?></a>
-		<?php echo __( "is the leading Enterprise Wordpress development company.", "ets_pmpro_discord" ); ?>
-		<?php echo __( "Contact us for any Wordpress Related development project.", "ets_pmpro_discord" ); ?>
-		<br> 
-		<span><b><?php echo __( "Email","ets_pmpro_discord" ); ?>: </b>
-		<a href="mailto:contact@expresstechsoftwares.com">contact@expresstechsoftwares.com</a> , 
-		<a href="mailto:business@expresstechsoftwares.com">business@expresstechsoftwares.com</a>
-		</span><br>
-		<span><b><?php echo __( "Skype","ets_pmpro_discord" ); ?>: </b>ravi.soni971</span><br>
-<span><b><?php echo __( "Phone/WhatsApp","ets_pmpro_discord" ); ?>: </b>+91-9806724185</span>
-	</div>
-</div>
-
-<div class="ets-support-lavel">
-	<div class="ets-supp-form">
-	  	<form accept="#" method="post">
-			<table class="form-table">
-				<tbody>						
-					<tr>
-						<th scope="row">
-							<?php echo __( "Full Name","ets_pmpro_discord" ); ?>	 
-						</th>
-						<td>
-							<input type="text" name="ets_user_name" placeholder="Enter Name" class="regular-text" required=""
-							value="<?php echo $currUserName;
-							 ?>">
-							<p class="description">
-								<?php echo __( "Write your full name","ets_pmpro_discord" );?>	
-							</p>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row"><?php echo __( "Contact Email","ets_pmpro_discord" );?> 
-						</th>
-						<td>
-							<input type="email" name="ets_user_email" placeholder=" Enter email" class="regular-text" required="" value="<?php echo get_option( 'admin_email' );
-							 ?>">
-							<p class="description"><?php echo __( "Write your contact email","ets_pmpro_discord" );?></p>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row">
-							<?php echo __( "Subject","ets_pmpro_discord" ); ?> 
-						</th>
-						<td>
-							<input type="text" name="ets_support_subject" placeholder=" Enter your subject" class="regular-text" required="">
-							<p class="description"><?php echo __( "Write your support subject","ets_pmpro_discord" );?></p>
-					
-						</td>
-					</tr>
-					<tr>
-						<th scope="row">
-						<?php echo __( "Message","ets_pmpro_discord" ); ?> 
-						</th>
-						<td>
-							<textarea name="ets_support_msg" rows="5" cols="50" required="" class="ets-regular-text"></textarea>
-							<p class="description"><?php echo __( "Write your support message","ets_pmpro_discord" );?></p>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<p class="submit">
-				<input type="submit" name="save" id="save" class="ets-submit" value="Send">
-			</p>
-		</form>
-	</div> 
+<div class="contact-form ">
+	<form accept="#" method="post">
+      <div class="ets-container">
+        <div class="top-logo-title">
+          <img src="https://www.expresstechsoftwares.com/wp-content/uploads/xcropped-2100-x1500-1.png.pagespeed.ic.DFnEHWLdQO.png" class="img-fluid company-logo" alt="">
+          <h1>ExpressTech Softwares Solutions Pvt Ltd</h1>
+          <p>ExpressTech Software Solution Pvt Ltd is the leading Enterprise Wordpress development company.<br>
+          Contact us for any Wordpress Related development projects.</p>
+        </div>
+        <div class="form-fields-box ">
+          <div class="ets-row ets-mt-5 ets-align-items-center">
+            <div class="ets-col-7 ets-offset-md-1">
+              <div class="contact-fields pr-100">
+                <div class="ets-form-group">
+                  <label>Full Name</label>
+                  <input type="text" name="ets_user_name" value="<?php echo $currUserName; ?>" class="form-control contact-input" placeholder="Write Your Full Name">
+                </div>
+                <div class="ets-form-group">
+                  <label>Contact Email</label>
+                  <input type="text" name="ets_user_email" class="form-control contact-input" value="<?php echo get_option( 'admin_email' );
+							 ?>" placeholder="Write Your Email">
+                </div>
+                <div class="ets-form-group">
+                  <label>Subject</label>
+                  <input type="text" name="ets_support_subject" class="form-control contact-input" placeholder="Write Your Subject">
+                </div>
+                <div class="ets-form-group">
+                  <label>Message</label>
+                  <textarea name="ets_support_msg" class="form-control contact-textarea">
+                    
+                  </textarea>
+                </div>
+                <div class="submit-btn d-flex align-items-center w-100 pt-3">
+                  <input type="submit" name="save" id="save" class="btn btn-submit" value="Submit">                  
+                  <a href="skype:ravi.soni971?chat" class="btn btn-skype ml-auto">Skype</a>
+                </div>
+              </div>
+            </div>
+            <div class="ets-col-3">
+              <div class="right-side-box">
+                <div class="contact-details d-inline-block w-100 mb-4">
+                  <div class="top-icon-title d-flex align-items-center w-100">
+                    <i class="fa fa-envelope-o title-icon" aria-hidden="true"></i>
+                    <p>Email</p>
+                  </div>
+                  <div class="contact-body mt-3">
+                    <p>contact@expresstechsoftwares.com</p>
+                    <p>business@expresstechsoftwares.com</p>
+                  </div>
+                </div>
+                <div class="contact-details d-inline-block w-100 mb-4">
+                  <div class="top-icon-title d-flex align-items-center w-100">
+                    <i class="fa fa-skype title-icon" aria-hidden="true"></i>
+                    <p>Skype</p>
+                  </div>
+                  <div class="contact-body mt-3">
+                    <p>ravi.soni971</p>
+                  </div>
+                </div>
+                <div class="contact-details d-inline-block w-100">
+                  <div class="top-icon-title d-flex align-items-center w-100">
+                    <i class="fa fa-whatsapp title-icon" aria-hidden="true"></i>
+                    <p>Whatsapp / No...</p>
+                  </div>
+                  <div class="contact-body mt-3">
+                    <p>+91-9806724185</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+	</form>
 </div>

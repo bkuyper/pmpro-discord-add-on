070d3be7ee938eb432593751df634d07d9e91de9
aashishets <aashish.meena.ets@gmail.com>
2021-02-17 09:37:12 +0000

    Description added on pmpro-discord-add-on.php

--- pmpro-discord-add-on.php
+++ pmpro-discord-add-on.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PaidMembershipsPro Discord Add-on
  * Plugin URI:  https://www.expresstechsoftwares.com
- * Description: 
+ * Description: Official PMPRO Add-on, it allows PMPRO Members to connect to any discord server.
  * Version: 1.0.0
  * Author: Strangers Studios & ExpressTech Software Solutions Pvt. Ltd.
  * Text Domain: ets_pmpro_discord

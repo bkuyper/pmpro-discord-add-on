8f2d22e8ffd65e9a1bcf22e09c78c8026798cf63
aashishmeenaets <aashish.meena@expresstechsoftwares.com>
2021-10-07 19:37:12 +0530

    #247

--- includes/pages/error_log.php
+++ includes/pages/error_log.php
@@ -9,15 +9,15 @@ while ( ! feof( $handle ) ) {
 	fclose( $handle );
 ?>
 </div>
-<div class="clrbtndiv">
+<div class="pmpro-clrbtndiv">
 	<div class="form-group">
-		<input type="button" class="clrbtn ets-submit ets-bg-red" id="clrbtn" name="clrbtn" value="Clear Logs !">
+		<input type="button" class="pmpro-clrbtn ets-submit ets-bg-red" id="pmpro-clrbtn" name="pmpro_clrbtn" value="Clear Logs !">
 		<span class="clr-log spinner" ></span>
 	</div>
 	<div class="form-group">
 		<input type="button" class="ets-submit ets-bg-green" value="Refresh" onClick="window.location.reload()">
 	</div>
 	<div class="form-group">
-		<a href="<?php echo esc_attr( content_url('/') . $filename ); ?>" class="ets-submit ets-bg-download" download><?php echo __( 'Download', 'pmpro-discord-add-on'  ); ?></a>
+		<a href="<?php echo esc_attr( content_url('/') . $filename ); ?>" class="ets-submit ets-pmpro-bg-download" download><?php echo __( 'Download', 'pmpro-discord-add-on'  ); ?></a>
 	</div>
 </div>

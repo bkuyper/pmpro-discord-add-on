4e4de77c0341ce74b2f40dbcf532ad2ecdfe4128
Ravi Soni <ravisoni6262@gmail.com>
2022-02-18 15:30:51 +0530

    Added check for option

--- pmpro-discord.php
+++ pmpro-discord.php
@@ -3,7 +3,7 @@
  * Plugin Name: Connect Paid Memberships Pro to Discord
  * Plugin URI:  https://www.expresstechsoftwares.com/step-by-step-documentation-guide-on-how-to-connect-pmpro-and-discord-server-using-discord-addon
  * Description: Connect your PaidMebershipPro site to your discord server, enable your members to be part of your community.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: ExpressTech Software Solutions Pvt. Ltd., Strangers Studios
  * Author URI: https://www.expresstechsoftwares.com
  * Text Domain: pmpro-discord-add-on
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // create plugin version constant.
-define( 'ETS_PMPRO_VERSION', '1.1.0' );
+define( 'ETS_PMPRO_VERSION', '1.1.1' );
 
 // create plugin url constant.
 define( 'ETS_PMPRO_DISCORD_URL', plugin_dir_url( __FILE__ ) );
@@ -94,7 +94,7 @@ class Ets_Pmpro_Add_Discord {
 		update_option( 'ets_pmpro_discord_log_api_response', false );
 		update_option( 'ets_pmpro_retry_failed_api', true );
 		update_option( 'ets_pmpro_discord_job_queue_concurrency', 1 );
-    update_option( 'ets_pmpro_member_kick_out', 0 );
+		update_option( 'ets_pmpro_member_kick_out', 0 );
 		update_option( 'ets_pmpro_discord_job_queue_batch_size', 7 );
 		update_option( 'ets_pmpro_allow_none_member', 'yes' );
 		update_option( 'ets_pmpro_retry_api_count', '5' );
